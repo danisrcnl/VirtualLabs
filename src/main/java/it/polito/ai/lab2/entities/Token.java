3e310650d636e1bdee28ddbb4e2d65d9bb8708f4
Daniele Sarcinella <s212122@studenti.polito.it>
2020-05-09 15:44:12 +0200

    #10 not working

--- src/main/java/it/polito/ai/lab2/entities/Token.java
+++ src/main/java/it/polito/ai/lab2/entities/Token.java
@@ -1,12 +1,16 @@
 package it.polito.ai.lab2.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.sql.Timestamp;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 @Entity
 @Data
