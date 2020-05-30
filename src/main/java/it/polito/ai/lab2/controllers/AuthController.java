e749828192163552d2eae19007cda2724637549d
Daniele Sarcinella <s212122@studenti.polito.it>
2020-05-30 16:52:02 +0200

    whole base for #11

---
+++ src/main/java/it/polito/ai/lab2/controllers/AuthController.java
@@ -0,0 +1,47 @@
+package it.polito.ai.lab2.controllers;
+
+
+import it.polito.ai.lab2.repositories.UserRepository;
+import it.polito.ai.lab2.security.AuthenticationRequest;
+import it.polito.ai.lab2.security.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+import javax.security.sasl.AuthenticationException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.http.ResponseEntity.ok;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    UserRepository users;
+
+    @PostMapping("/signin")
+    public ResponseEntity signin(@RequestBody AuthenticationRequest data) {
+        String username = data.getUsername();
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, data.getPassword()));
+        String token = jwtTokenProvider.createToken(username, this.users.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Username " + username + "not found")).getRoles());
+        Map<Object, Object> model = new HashMap<>();
+        model.put("username", username);
+        model.put("token", token);
+        return ok(model);
+    }
+}
