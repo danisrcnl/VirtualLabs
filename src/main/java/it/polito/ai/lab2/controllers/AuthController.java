ca8fe8c8870601199a1143d6617e4f2d7e1921ea
Daniele <danielesarcinella@outlook.com>
2021-03-02 11:13:38 +0100

    sign up implementation

--- src/main/java/it/polito/ai/lab2/controllers/AuthController.java
+++ src/main/java/it/polito/ai/lab2/controllers/AuthController.java
@@ -1,9 +1,13 @@
 package it.polito.ai.lab2.controllers;
 
 
+import it.polito.ai.lab2.dataStructures.SignUpRequest;
+import it.polito.ai.lab2.dtos.StudentDTO;
 import it.polito.ai.lab2.repositories.UserRepository;
 import it.polito.ai.lab2.security.AuthenticationRequest;
 import it.polito.ai.lab2.security.JwtTokenProvider;
+import it.polito.ai.lab2.services.AuthenticationService;
+import it.polito.ai.lab2.services.TeamService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -33,11 +37,19 @@ public class AuthController {
     JwtTokenProvider jwtTokenProvider;
 
     @Autowired
+    AuthenticationService authenticationService;
+
+    @Autowired
     UserRepository users;
+
+    @Autowired
+    TeamService teamService;
 
     @PostMapping("/signin")
     public ResponseEntity signin(@RequestBody AuthenticationRequest data) {
         try {
+            System.out.println(data.getUsername());
+            System.out.println(data.getPassword());
             String username = data.getUsername();
             authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, data.getPassword()));
             String token = jwtTokenProvider.createToken(username, this.users.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Username " + username + "not found")).getRoles());
@@ -48,5 +60,20 @@ public class AuthController {
         } catch(AuthenticationException e) {
             throw new BadCredentialsException("Invalid username/password supplied");
         }
+    }
+
+    @PostMapping("/signup")
+    public void signup(@RequestBody SignUpRequest signUpRequest) {
+        authenticationService.addUser(signUpRequest.getEmail(), signUpRequest.getPassword());
+        StudentDTO studentDTO = StudentDTO
+                .builder()
+                .name(signUpRequest.getLastName())
+                .firstName(signUpRequest.getFirstName())
+                .email(signUpRequest.getEmail())
+                .photoPath("")
+                .id(signUpRequest.getId())
+                .build();
+
+        teamService.addStudent(studentDTO);
     }
 }
---
+++ src/main/java/it/polito/ai/lab2/services/AuthenticationServiceImpl.java
@@ -0,0 +1,22 @@
+package it.polito.ai.lab2.services;
+
+import it.polito.ai.lab2.entities.User;
+import it.polito.ai.lab2.entities.UserExistingException;
+import it.polito.ai.lab2.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class AuthenticationServiceImpl implements AuthenticationService{
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public void addUser(String username, String password) throws UserExistingException {
+        if(userRepository.findByUsername(username).isPresent())
+            throw new UserExistingException(username);
+        User u = new User();
+        u.setUsername(username);
+        u.setPassword(password);
+        userRepository.save(u);
+    }
+}
