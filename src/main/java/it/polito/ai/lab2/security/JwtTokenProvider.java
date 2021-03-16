2b48269ee38dc6d065779d9946e26b474d457792
Daniele Sarcinella <s212122@studenti.polito.it>
2021-03-16 12:06:11 +0100

    fix

--- src/main/java/it/polito/ai/lab2/security/JwtTokenProvider.java
+++ src/main/java/it/polito/ai/lab2/security/JwtTokenProvider.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
