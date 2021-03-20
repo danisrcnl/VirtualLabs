9ac30afbb4d86143da98fe02e7d8137d72139f12
Daniele Sarcinella <s212122@studenti.polito.it>
2021-03-20 10:25:33 +0100

    fix

--- src/main/java/it/polito/ai/lab2/services/AssignmentService.java
+++ src/main/java/it/polito/ai/lab2/services/AssignmentService.java
@@ -10,42 +10,42 @@ import java.util.Optional;
 
 public interface AssignmentService {
 
-    Long addAssignment(AssignmentDTO assignment);
+    Long addAssignment (AssignmentDTO assignment);
 
-    Optional<AssignmentDTO> getAssignment(Long id);
+    Optional<AssignmentDTO> getAssignment (Long id);
 
-    List<AssignmentDTO> getAllAssignments();
-
-
-    List<AssignmentDTO> getCourseAssignments(String courseName);
-
-    Long addPaper(PaperDTO paper, String courseName, String teamName, Long assignmentId);
-
-    boolean linkPaperToAssignment(Long paperId, Long assignmentId);
-
-    Optional<PaperDTO> getPaper(Long id);
-
-    List<PaperDTO> getPapersForTeam(String courseName, String teamName);
-
-    List<PaperDTO> getPapersForAssignment(Long id);
-
-    List<PaperDTO> getAllPapers();
-
-    boolean ratePaper(Long paperId, int mark);
-
-    String initializePaperStatus(Long paperId);
-
-    String readPaper(Long paperId);
-
-    void reviewPaper(Long paperId);
-
-    void deliverPaper(Long paperId);
-
-    void setPaperContent(Long paperId, String content);
+    List<AssignmentDTO> getAllAssignments ();
 
 
-    Long addPaperStatusTime(PaperStatusTimeDTO paperStatusTimeDTO, Long paperId);
+    List<AssignmentDTO> getCourseAssignments (String courseName);
 
-    List<PaperStatusTimeDTO> getPaperHistory(Long paperId);
+    Long addPaper (PaperDTO paper, String courseName, String teamName, Long assignmentId);
+
+    boolean linkPaperToAssignment (Long paperId, Long assignmentId);
+
+    Optional<PaperDTO> getPaper (Long id);
+
+    List<PaperDTO> getPapersForTeam (String courseName, String teamName);
+
+    List<PaperDTO> getPapersForAssignment (Long id);
+
+    List<PaperDTO> getAllPapers ();
+
+    boolean ratePaper (Long paperId, int mark);
+
+    String initializePaperStatus (Long paperId);
+
+    String readPaper (Long paperId);
+
+    void reviewPaper (Long paperId);
+
+    void deliverPaper (Long paperId);
+
+    void setPaperContent (Long paperId, String content);
+
+
+    Long addPaperStatusTime (PaperStatusTimeDTO paperStatusTimeDTO, Long paperId);
+
+    List<PaperStatusTimeDTO> getPaperHistory (Long paperId);
 
 }
